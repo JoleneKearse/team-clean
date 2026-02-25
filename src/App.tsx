43ea3b4749f1a8178c78c3e2b8922e7811b45307
JoleneKearse <pharaohnutz@gmail.com>
2026-02-25 12:52:12 -0500

    chore: add additional instruction for staffing block

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,11 @@ function App() {
     <div className="flex flex-col items-center gap-4 p-4">
       <section className="w-full border border-gray-500 overflow-hidden rounded-xl shadow-lg p-4 bg-gray-200">
         <div className="flex items-center justify-between gap-4">
-          <h2 className="font-semibold">Who is in today?</h2>
+          <div>
+            <h2 className="font-semibold">Who is in today?</h2>
+            <p className="text-sm italic">Uncheck names if necessary</p>
+          </div>
+          
           <span className="font-semibold">Staffing: {peopleIn}</span>
         </div>
 
